2664eb132e2f6917066c5341769f1b02e4a7e59b
Sourav Das <sdunity12061999@gmail.com>
2022-03-08 09:31:21 +0530

    editor complete

--- Page Components/Admin Editor/wisywig.jsx
+++ Page Components/Admin Editor/wisywig.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 // import { Editor } from "react-draft-wysiwyg";
 import dynamic from "next/dynamic";
-
+import { Button } from "@mui/material";
+import { MdDone } from "react-icons/md";
+import { EditorState } from "draft-js";
 const Editor = dynamic(
   () => import("react-draft-wysiwyg").then((module) => module.Editor),
   {
@@ -10,12 +12,23 @@ const Editor = dynamic(
   }
 );
 function Wisywig() {
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  console.log(Editor.createEmpty);
   return (
     <div>
       <Editor
+        editorState={editorState}
         toolbarClassName={"wysiwig-toolbar"}
         editorClassName={"wysiwig-editor"}
+        onEditorStateChange={(editorState) => {
+          setEditorState(editorState);
+        }}
       />
+      <div style={{ width: "100px", margin: "10px auto" }}>
+        <Button variant="contained" startIcon={<MdDone />}>
+          Publish
+        </Button>
+      </div>
     </div>
   );
 }
